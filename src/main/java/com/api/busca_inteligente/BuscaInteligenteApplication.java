9feaee9cdfd7faf06c13d426e3938c712fc50956
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-07-01 12:54:59 -0300

    chore: inicializar estrutura base do projeto spring boot

---
+++ src/main/java/com/api/busca_inteligente/BuscaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.api.busca_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/busca_inteligente/BuscaInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.busca_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
